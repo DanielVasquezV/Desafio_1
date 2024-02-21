97217e87792071a8ecb9669fb409cbc03d546e7f
Menendez2004 <nitrotruelo@gmail.com>
2024-02-20 22:57:39 -0600

    Clases dentro de paquete libros empezada y avanzada

--- src/sv/edu/udb/desafio1/Main.java
+++ src/sv/edu/udb/desafio1/Main.java
@@ -1,9 +1,53 @@
 package sv.edu.udb.desafio1;
-
+import sv.edu.udb.desafio1.material.escrito.Libro;
+import java.util.HashMap;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "LOL");
+//        Mediateca mediateca = new Mediateca();
+        //Para almacenar los libros
+        int opcion;
+
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "1-Agregar\n 2-Modificar\n " +
+                                                                        "3-Listar\n 4-Borrar\n 5-Buscar\n 6-Salir"));
+
+            try{//try para validar que el usuario coloque una variable numerica y no ponga un string
+                switch (opcion){
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+
+
+                    case 6:
+
+                        break;
+                }
+
+            }catch (NumberFormatException e)//convierte una cadena no numerica a numero
+            {
+                JOptionPane.showMessageDialog(null, "Opción no válida. Por favor, ",
+                                              "ingresa un número del 1 al 6", JOptionPane.WARNING_MESSAGE);
+
+            }
+
+
+        }while (opcion !=6);
     }
 }
--- src/sv/edu/udb/desafio1/material/escrito/Libro.java
+++ src/sv/edu/udb/desafio1/material/escrito/Libro.java
@@ -1,8 +1,103 @@
 package sv.edu.udb.desafio1.material.escrito;
+import javax.swing.*;
+import java.util.HashMap;
 
 public class Libro extends MaterialEscrito {
+    private String titulo;
     private String autor;
     private int numeroPaginas;
-    private String isbn;
-    private int anoPublicacion;
-}
+    private String editorial;
+    private String ISBN;
+    private int anioPublicacion;
+    private int unidadesDisponibles;
+
+    public Libro(String titulo, String autor, int numeroPaginas, String editorial, String ISBN, int anioPublicacion,
+                 int unidadesDisponibles){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+        this.editorial = editorial;
+        this.ISBN = ISBN;
+        this.anioPublicacion = anioPublicacion;
+        this.unidadesDisponibles = unidadesDisponibles;
+    }
+     //seter y getter
+     public String getTitulo() {
+         return titulo;
+     }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public int getUnidadesDisponibles() {
+        return unidadesDisponibles;
+    }
+
+    public void setUnidadesDisponibles(int unidadesDisponibles) {
+        this.unidadesDisponibles = unidadesDisponibles;
+    }
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", numeroPaginas=" + numeroPaginas +
+                ", editorial='" + editorial + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                ", anioPublicacion=" + anioPublicacion +
+                ", unidadesDisponibles=" + unidadesDisponibles +
+                '}';
+    }
+
+    class Mediateca {
+        private HashMap<Integer, Libro> libros;
+
+        public Mediateca(){
+            this.libros = new HashMap<>();
+        }
+        public void addBooks(Libro libro){
+            libros.put(libros.size() +1, libro);
+            JOptionPane.showMessageDialog(null, "Libro agregado correctamente.");
+        }
+        }
+    }
